00eda2e4a1fed87ffff95a8c1ae2b0ca6fda6ef4
M-Luo <M-Luo@users.noreply.github.com>
2019-05-12 22:34:03 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import lexer
+import parser
+
+def main():
+  # read source file
+  source=""
+  with open('toy.lang','r') as file:
+    source = file.read()
+  
+  # Lexer
+  # call lexer class and initialize it with source code
+  lex = lexer.Lexer(source)
+  # call tokenizer
+  tokens = lex.tokenize()
+
+  # Paser
+  parse = parser.Parser(tokens)
+  parse.parse()
+  
+main()
