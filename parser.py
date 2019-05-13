7be67c6713e9899feae42ac4781cebe9c81f2d0c
M-Luo <M-Luo@users.noreply.github.com>
2019-05-13 12:43:08 -0400

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,1 +1,14 @@
+import ply.yacc as yacc
+from lexer import tokens
 
+def expr_plus(p):
+  'expression : expression PLUS term'
+  p[0] = p[1] + p[3]
+
+def expr_minus(p):
+  'expression : expression MINUS term'
+  p[0] = p[1] - p[3]
+
+def expr_mul(p):
+  'expression : expression MUL term'
+  p[0] = p[1] * p[3]
